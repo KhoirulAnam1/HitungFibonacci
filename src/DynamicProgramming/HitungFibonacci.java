b0a78b5bde872c1031f87688ec9febba37e8a42a
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-09-10 10:09:00 +0700

    Tambah Hitung Fibonacci

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -5,6 +5,7 @@
  */
 package DynamicProgramming;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -19,6 +20,7 @@ public class HitungFibonacci {
         tampilJudul(identitas);
         
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
     
     private static void tampilJudul(String identitas)
@@ -36,6 +38,20 @@ public class HitungFibonacci {
         int n = scanner.nextInt();
         
         return n;
+    }
+    
+    private static BigInteger fibo(int n) {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
     }
        
 }
