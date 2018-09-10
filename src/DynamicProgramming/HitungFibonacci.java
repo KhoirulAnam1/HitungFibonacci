0159b79985662675ebe27b2fa5fd2bc0b8f13105
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-09-10 10:02:22 +0700

    Initial Commit

---
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DynamicProgramming;
+
+/**
+ *
+ * @author M E G A S T A R
+ */
+public class HitungFibonacci {
+    public static void main(String[] args)
+    {
+        String identitas = "Muhammad Khoirul Anam / XRPL3 / 25";
+        
+        tampilJudul(identitas);
+    }
+    
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+       
+}
