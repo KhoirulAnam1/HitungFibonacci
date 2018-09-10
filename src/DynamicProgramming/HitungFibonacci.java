5cebe753faac828fac5cf21d02ac3c9291aaf193
M E G A S T A R <M E G A S T A R@LAPTOP-VFMFVDH5>
2018-09-10 10:12:02 +0700

    Tambah Tampil Input

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -21,6 +21,7 @@ public class HitungFibonacci {
         
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
     
     private static void tampilJudul(String identitas)
@@ -52,6 +53,10 @@ public class HitungFibonacci {
         }
         
         return hasil[n-1];
+    }
+    
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
     }
        
 }
